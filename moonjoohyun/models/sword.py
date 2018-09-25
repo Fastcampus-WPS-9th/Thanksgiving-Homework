989dd3f3d34d0e9f95583caac3fe2d6362da23f0
ElonMoon <elonmoon@gmail.com>
2018-09-25 22:22:49 +0900

    models add attr

--- moonjoohyun/models/sword.py
+++ moonjoohyun/models/sword.py
@@ -1,3 +1,6 @@
 class Sword:
     def __init__(self, name):
         self.name = name
+        self.probability = 0
+        self.enchant_value = 0
+        self.broken = False
--- moonjoohyun/models/user.py
+++ moonjoohyun/models/user.py
@@ -1,6 +1,5 @@
 class User:
     def __init__(self, name):
         self.name = name;
-
-    def do_enchant(self, sword):
-        pass
+        self.probability = 0
+        self.spell_count = 0
